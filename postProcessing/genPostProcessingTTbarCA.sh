b91a78d45c4afecfe46a4824651e27fc29093347
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-04-25 20:23:39 +0200

    models

---
+++ postProcessing/genPostProcessingTTbarCA.sh
@@ -0,0 +1,26 @@
+python genPostProcessingTTbarCA.py --addEFTWeights --addGenWeights --genTTbar --sample TT01j2lCAOldRef_Mtt500_ext #SPLIT3000 
+
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --sample TTLep_pow #SPLIT2000
+#
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --genTTbar --sample TTLep_hdampDOWN #SPLIT800
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --genTTbar --sample TTLep_hdampUP #SPLIT800
+
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --fakeB 1 --sample ST_tW_top #SPLIT200
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --fakeB 1 --sample ST_tW_antitop #SPLIT200
+
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT100to200 #SPLIT300
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT200to400 #SPLIT250
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT400to600 #SPLIT200
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT600to800 #SPLIT200
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT800to1200 #SPLIT200
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT1200to2500 #SPLIT150
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --removeDelphesFiles --fakeB 2 --sample DYJetsToLL_M50_HT2500toInf #SPLIT100
+
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --genTTbar --sample TTsemilep_pow #SPLIT3000
+
+#python genPostProcessingTTbarCA.py  --addEFTWeights --sample TT01j2lCAMyRef #SPLIT400
+#python genPostProcessingTTbarCA.py  --addEFTWeights --sample TT01j2lCAOldRef_HT300 #SPLIT327
+#python genPostProcessingTTbarCA.py  --addEFTWeights --sample ST_tW01j2lCAMyRef #SPLIT200
+
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --sample DYJetsToLL_M10To50 #SPLIT800
+#python genPostProcessingTTbarCA.py --addGenWeights --miniAOD --sample DYJetsToLL_M50 #SPLIT2000
