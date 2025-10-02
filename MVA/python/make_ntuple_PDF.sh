72ceb4ce5b3ed32e026b63e3383ddc5117413029
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2025-10-02 21:32:11 +0200

    PDF

--- MVA/python/make_ntuple_PDF.sh
+++ MVA/python/make_ntuple_PDF.sh
@@ -1,6 +1,6 @@
-python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Autumn18
-python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Summer16_preVFP
-python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Summer16
-python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Fall17
+#python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Autumn18
+#python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Summer16_preVFP
+#python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Summer16
+#python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep_Fall17
 
 python make_ntuple.py --output /scratch-cbe/users/robert.schoefbeck/TT2lUnbinned/training-ntuples-v7 --config PDF --sample TTLep
